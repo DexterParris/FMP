60c40912e03e49260aa7a3bbf1e68778613de2d4
DexterParris <91732338+DexterParris@users.noreply.github.com>
2022-05-06 14:45:13 +0100

    cctv script added

---
+++ Assets/Scripts/Core/CCTVScript.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTVScript : MonoBehaviour
+{
+    public GameObject CAM1,CAM2,CAM3,CAM4,CAM5,CAM6,CAM7,CAM8,CAM9,mainCam;
+    public static string currentCAM;
+    public GameObject map;
+    public static bool camsOpen; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camsOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleCams()
+    {
+        if(camsOpen == false)
+        {
+            camsOpen = true;
+            mainCam.SetActive(false);
+            map.SetActive(true);
+            StartCoroutine(ResetCams());
+            if(currentCAM == "CAM1")
+            {
+                CAM01();
+            }
+            else if(currentCAM == "CAM2")
+            {
+                CAM02();
+            }
+            else if(currentCAM == "CAM3")
+            {
+                CAM03();
+            }
+            else if(currentCAM == "CAM4")
+            {
+                CAM04();
+            }
+            else if(currentCAM == "CAM5")
+            {
+                CAM05();
+            }
+            else if(currentCAM == "CAM6")
+            {
+                CAM06();
+            }
+            else if(currentCAM == "CAM7")
+            {
+                CAM07();
+            }
+            else if(currentCAM == "CAM8")
+            {
+                CAM08();
+            }
+            else if(currentCAM == "CAM9")
+            {
+                CAM09();
+            }
+        }
+        else
+        {
+            camsOpen = false;
+            mainCam.SetActive(true);
+            map.SetActive(false);
+            StartCoroutine(ResetCams());
+        }
+        
+    }
+
+    IEnumerator ResetCams()
+    {
+        CAM1.SetActive(false);
+        CAM2.SetActive(false);
+        CAM3.SetActive(false);
+        CAM4.SetActive(false);
+        CAM5.SetActive(false);
+        CAM6.SetActive(false);
+        CAM7.SetActive(false);
+        CAM8.SetActive(false);
+        CAM9.SetActive(false);
+        yield return null;
+    }
+
+    public void CAM01()
+    {
+        StartCoroutine(ResetCams());
+        CAM1.SetActive(true);
+        currentCAM = "CAM01";
+    }
+
+    public void CAM02()
+    {
+        StartCoroutine(ResetCams());
+        CAM2.SetActive(true);
+        currentCAM = "CAM2";
+    }
+    public void CAM03()
+    {
+        StartCoroutine(ResetCams());
+        CAM3.SetActive(true);
+        currentCAM = "CAM3";
+    }
+    public void CAM04()
+    {
+        StartCoroutine(ResetCams());
+        CAM4.SetActive(true);
+        currentCAM = "CAM4";
+    }
+    public void CAM05()
+    {
+        StartCoroutine(ResetCams());
+        CAM5.SetActive(true);
+        currentCAM = "CAM5";
+    }
+    public void CAM06()
+    {
+        StartCoroutine(ResetCams());
+        CAM6.SetActive(true);
+        currentCAM = "CAM6";
+    }
+    public void CAM07()
+    {
+        StartCoroutine(ResetCams());
+        CAM7.SetActive(true);
+        currentCAM = "CAM7";
+    }
+    public void CAM08()
+    {
+        StartCoroutine(ResetCams());
+        CAM8.SetActive(true);
+        currentCAM = "CAM8";
+    }
+    public void CAM09()
+    {
+        StartCoroutine(ResetCams());
+        CAM9.SetActive(true);
+        currentCAM = "CAM9";
+    }
+    
+
+}
--- Assets/Scripts/Core/TimeScript.cs
+++ Assets/Scripts/Core/TimeScript.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TimeScript : MonoBehaviour
 {
+    public Animator anim;
     public static int currentTime;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI nightText;
     // Start is called before the first frame update
     void Start()
     {
-        currentTime = 12;
-        InvokeRepeating("ChangeTime", 90f, 89f);
+        nightText.text = PlayerPrefs.GetString("currentNight");
+        currentTime = 0;
+        InvokeRepeating("ChangeTime", 9f, 8f);
     }
 
     // Update is called once per frame
@@ -20,19 +26,62 @@ public class TimeScript : MonoBehaviour
 
     public void ChangeTime()
     {
-        if(currentTime == 12)
+        if(currentTime == 5)
         {
-            currentTime = 01;
-        }
-        else if(currentTime == 6)
-        {
-            currentTime = 06;
+            currentTime = 6;
+            timeText.text = ("0"+currentTime+":AM");
+            if(PlayerPrefs.GetString("currentNight")=="Night 1")
+            {
+                PlayerPrefs.SetString("currentNight","Night 2");
+                StartCoroutine(ChangeScene());
+            }
+            else if(PlayerPrefs.GetString("currentNight")=="Night 2")
+            {
+                PlayerPrefs.SetString("currentNight","Night 3");
+                StartCoroutine(ChangeScene());
+            }
+            else if(PlayerPrefs.GetString("currentNight")=="Night 3")
+            {
+                PlayerPrefs.SetString("currentNight","Night 4");
+                StartCoroutine(ChangeScene());
+            }
+            else if(PlayerPrefs.GetString("currentNight")=="Night 4")
+            {
+                PlayerPrefs.SetString("currentNight","Night 5");
+                StartCoroutine(ChangeScene());
+            }
+            else if(PlayerPrefs.GetString("currentNight")=="Night 5")
+            {
+                PlayerPrefs.SetString("currentNight","Night 6");
+                StartCoroutine(ChangeScene());
+            }
+            else if(PlayerPrefs.GetString("currentNight")=="Night 6")
+            {
+                PlayerPrefs.SetString("currentNight","Night 7");
+                StartCoroutine(ChangeScene());
+            }
+            else if(PlayerPrefs.GetString("currentNight")=="Night 7")
+            {
+                PlayerPrefs.SetString("currentNight","Night 7");
+                StartCoroutine(ChangeScene());
+            }
             
         }
         else
         {
-            currentTime += 01;
+            currentTime += 1;
+            timeText.text = ("0"+currentTime+":AM");
         }
         
+    }
+
+
+    IEnumerator ChangeScene()
+    {
+        anim.SetTrigger("ToBlack");
+        
+        yield return new WaitForSeconds(1f);
+        
+        SceneManager.LoadScene("Main Menu");
     }
 }
